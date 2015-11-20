5785d14f4ea22f1e3c5ddb35764e5998932e54c7
Ana Sofia Maia <sofymaia@sofys-mbp.middlebury.edu>
2015-11-20 16:33:27 -0500

    add resultlist display for requirements & integrate with dept search. can filter by 1 dept & 1 req

--- public/js/views/main_view.js
+++ public/js/views/main_view.js
@@ -1,16 +1,31 @@
 window.onload = function displayResults(){
-  var depts = document.getElementById("dept_list").children;
-  depts = Array.from(depts);
-  depts.forEach(function(elmnt){
+  //get qualifier lists
+  var depts = document.getElementById("dept_list");
+  var reqs = document.getElementById("req_list");
+  var qualifsArr = Array.from(depts.children).concat(Array.from(reqs.children));
+
+  var addClick = function(elmnt){
+    //add event listener to all depts & reqs, build querystring from currently selected elements
     elmnt.addEventListener("click", function(){
-      getData('subject='+elmnt.id, elmnt);
-    });
-  });
+
+      if(elmnt.className !== 'list-group-item clicked'){elmnt.className = 'list-group-item clicked';}
+      else{elmnt.className = 'list-group-item';}
+
+      var querystring = 'query?';
+      var selected = Array.from(depts.getElementsByClassName('list-group-item clicked'))
+             .concat(Array.from(reqs.getElementsByClassName('list-group-item clicked')));
+
+      selected.forEach(function(selItem){querystring += selItem.id;});
+
+      getData(querystring);});
+  };
+
+  qualifsArr.forEach(function(elmnt){addClick(elmnt);});
 };
 
-var getData = function(url, dept){
+var getData = function(url){
   var request = new XMLHttpRequest();
-  var path = 'http://localhost:8000/api/catalog/query?'+url;
+  var path = 'http://localhost:8000/api/catalog/'+url;
   request.open('GET', path, true);
   request.setRequestHeader("Content-Type", "text/xml");
   request.onreadystatechange = function(){
@@ -26,12 +41,12 @@ var displayCourses = function(courses){
   var resultList = document.getElementById("courses");
   resultList.innerHTML = "";
 
-  var headerTemplate = "<div class='course-panel-heading'>" +
-      "<h5 style='color:white'>#TITLE#</h5><h6 style='color:white' align=left>#CODE#</h6>" +
-      "<h6 style='color:white' align=left>#PROFESSOR#</h6></div>";
+  var headerTemplate = "<div class='course-panel-heading'><table><tr>" +
+      "<td style='width:650px'><h5 style='color:white'>#TITLE#</h5></td>" +
+      "<td style='width:200px'><h6 style='color:white' align=right>#CODE#</h6></td></tr></table></div>";
   var bodyTemplate = "<div class='course-panel-body'><div class='row'>" +
-          "<div class='col-lg-12'><table class='table'><tr><td colspan=2>#DESCRIPTION#</td></tr>" +
-          "<tr><td>Location: #LOCATION#</td><td>Schedule: #SCHEDULE#</td></tr>" +
+          "<div class='col-lg-12'><table class='table'><tr><td colspan=2>#PROFESSOR#</td></tr>" +
+          "<tr><td colspan=2>#DESCRIPTION#</td></tr><tr><td>Location: #LOCATION#</td><td>Schedule: #SCHEDULE#</td></tr>" +
           "<tr><td>Requirements: #REQUIREMENTS#</td><td>CRN: #CRN#</td></tr><tr><td colspan=2 align=center>" +
           "<a href=#HREF# class='btn btn-primary'>Catalog Entry</a></td></tr></table></div></div></div>";
 
@@ -41,11 +56,11 @@ var displayCourses = function(courses){
       var property = substring.slice(1,-1);
       if(property === 'TITLE'){return crs.title;}
       else if(property === 'CODE'){return crs.code;}
-      else if(property === 'PROFESSOR'){return crs.instructor;}
     });
     var fullContents = bodyTemplate.replace(/#[^#]*#/g,function(substring){
       var property = substring.slice(1,-1);
-      if(property === 'DESCRIPTION'){return crs.description;}
+      if(property === 'PROFESSOR'){return crs.instructor;}
+      else if(property === 'DESCRIPTION'){return crs.description;}
       else if(property === 'LOCATION'){return crs.location;}
       else if(property === 'SCHEDULE'){return crs.schedule;}
       else if(property === 'REQUIREMENTS'){return crs.requirements;}
@@ -54,17 +69,17 @@ var displayCourses = function(courses){
     });
 
     var temp = document.createElement('div');
-    temp.className='col-md-9';
-    temp.setAttribute("style","padding-top:.5cm"); temp.setAttribute("id","results");
+    temp.className='col-md-12';
+    temp.setAttribute("style","padding-top:.35cm");
     temp.innerHTML = contents;
 
     temp.addEventListener("click",function(){
-      if(temp.className !== 'col-md-9 open'){
-        temp.className = 'col-md-9 open';
+      if(temp.className !== 'col-md-12 open'){
+        temp.className = 'col-md-12 open';
         temp.innerHTML = contents + fullContents;
       }else{
         temp.innerHTML = contents;
-        temp.className = 'col-md-9';}
+        temp.className = 'col-md-12';}
     });
 
     resultList.appendChild(temp);
