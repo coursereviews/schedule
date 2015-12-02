df31d7a5ecc3a365912bdb9477d8d835dbdee8a2
Paolo Bernasconi <paolo@plaid.com>
2015-12-01 22:22:38 -0500

    fix rebase

--- public/js/views/extracurricular-main-view.js
+++ public/js/views/extracurricular-main-view.js
@@ -5,8 +5,6 @@ var app = app || {};
 
   app.views.ExtraCurricularMainView = Backbone.View.extend({
     initialize: function() {
-
-<<<<<<< 22faf8cdcf3344394834697d82ca3336e895329c
 			this.listenTo(schedule.extracurriculars, 'reset', this.addAll);
 		},
 		render: function() {
@@ -16,22 +14,8 @@ var app = app || {};
 
 		},
 		addAll: function() {
-			
+
 			schedule.extracurriculars.each(this.addOne, this);
 		}
 	});
-=======
-      this.listenTo(app.collections.extracurriculars, 'reset', this.render);
-    },
-    render: function() {
-
-    },
-    addOne: function() {
-
-    },
-    addAll: function() {
-
-    }
-  });
->>>>>>> temp
 })();
