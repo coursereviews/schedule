8792d373170a01f9b5feec5bfff11f54e6697749
Julian Billings <jabillings@gmail.com>
2015-12-08 01:12:39 -0500

    get initial tests working for favorites API

--- test/test_favorite_api.js
+++ test/test_favorite_api.js
@@ -1,6 +1,9 @@
-const assert = require('assert');
-const http = require('http');
+const request = require('supertest');
+const app = require('../lib/server').app;
+const express = require('express');
 const favoriteAPI = require('../lib/routes/favorite/favorite.controller');
+const User = require('../lib/models/user');
+const assert = require('assert');
 
 /*
  NOTE: This test should work with the current Fall 2015 catalog that gets scraped.
@@ -8,28 +11,50 @@ const favoriteAPI = require('../lib/routes/favorite/favorite.controller');
  Additionally, this test code only works if the favorite table is already empty.
 
 */
+/*
+request(app)
+.post('/api/favorites')
+.field('email', 'jabillings@middlebury.edu')
+.field('password', 'julian')
+.end(function(err, res) {
+  if (err) console.log(err);
+  console.log(res.cookie);
+});
+*/
 
-exports.runTests = function(user){
-
-  favoriteAPI.getAllFavoritesHelper(user)
-  .then(function(result) {
-    assert.strictEqual(result.models.length, 0, "department_id search not working for courses");
-    return favoriteAPI.addFavoriteHelper(user, 1);
-  }).then(function() {
-    return favoriteAPI.getAllFavoritesHelper(user);
-  }).then(function(result) {
-    assert.strictEqual(result.models.length, 1, "department_id search not working for courses");
-    return favoriteAPI.removeFavoriteHelper(user, 1);
-  }).then(function() {
-    return favoriteAPI.getAllFavoritesHelper(user);
-  }).then(function(result) {
-    assert.strictEqual(result.models.length, 0, "department_id search not working for courses");
-    return favoriteAPI.removeFavoriteHelper(user, -1);
-  }).then(function() {
-    return favoriteAPI.getAllFavoritesHelper(user);
-  }).then(function(result) {
-    assert.strictEqual(result.models.length, 0, "department_id search not working for courses");
-    resolve(true);
+runTests = function(email){
+  return new Promise(function(resolve, reject) {
+    var user;
+    var usr = new User(email).fetch()
+    .then(function(usr) {
+      user = usr;
+      return favoriteAPI.getAllFavoritesHelper(user);
+    }).then(function(result) {
+      assert.strictEqual(result.models.length, 0, "empty favorites DB not returning 0 results");
+      return favoriteAPI.addFavoriteHelper(user, 1);
+    }).then(function() {
+      return favoriteAPI.getAllFavoritesHelper(user);
+    }).then(function(result) {
+      assert.strictEqual(result.models.length, 1, "adding course doesn't work");
+      return favoriteAPI.removeFavoriteHelper(user, 1);
+    }).then(function() {
+      return favoriteAPI.getAllFavoritesHelper(user);
+    }).then(function(result) {
+      assert.strictEqual(result.models.length, 0, "removing course doesn't work");
+      resolve(true);
+    }).catch(function(err) {
+      console.log(err);
+      reject(Error(err));
+    });
   });
 
 }
+
+runTests('jabillings@middlebury.edu')
+.then(function(result) {
+  if (result === true) {
+    console.log("Favorite Tests Passed!");
+  } else {
+    console.log("Tests Failed");
+  }
+})
