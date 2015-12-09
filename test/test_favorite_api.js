e6e63dda416d18894836fe06e39d47da9d67ffce
Julian Billings <jabillings@gmail.com>
2015-12-09 16:46:25 -0500

    refactor some favorite api code, push test code to be adjusted later

--- test/test_favorite_api.js
+++ test/test_favorite_api.js
@@ -5,6 +5,8 @@ const express = require('express');
 const favoriteAPI = require('../lib/routes/favorite/favorite.controller');
 const User = require('../lib/models/user');
 const assert = require('assert');
+const request = require('supertest');
+const mocha = require('mocha');
 
 /*
  NOTE: This test should work with the current Fall 2015 catalog that gets scraped.
@@ -51,3 +53,40 @@ runFavoriteTests('jabillings@middlebury.edu')
 }).catch(function(err) {
   console.log("Tests Failed");
 });
+
+//console.log(Object.keys(request(app)));
+/*
+var usr = agent.agent();
+
+usr.post('http://localhost:8000/login')
+.send({email: 'jabillings@middlebury.edu', password: 'julian'})
+.end(function(err, res) {
+  //console.log(res);
+  //console.log(res);
+});
+
+usr.get('http://localhost:8000/api/catalog/query/courseoffering?crn=91423')
+.end(function(err, res) {
+  console.log(res.body);
+});*/
+
+
+  var agent = request.agent(app);
+  agent
+  .get('/login', function(req, res) {
+    console.log(res.status);
+    res.on('data', function(data){
+      console.log('here');
+    })
+    res.cookie('cookie', 'hey');
+    res.send();
+  });
+
+  agent
+  .post('/login')
+  .field('email', 'jabillings@middlebury.edu')
+  .field('password', 'julian')
+  .end(function (err, res) {
+    console.log(res.status);
+    console.log(res.headers['set-cookie']);
+  });
