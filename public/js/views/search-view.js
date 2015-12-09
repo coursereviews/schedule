56f2f2dae5c1155cc7113e2db33cbfcc5a64d7d9
Ana Sofia Maia <sofymaia@sofys-mbp.middlebury.edu>
2015-12-08 22:32:05 -0500

    added: click for details, schedulesearch html, search bars&selectsearch work, star glyph

--- public/js/views/search-view.js
+++ public/js/views/search-view.js
@@ -15,37 +15,46 @@ var app = app || {};
     },
 
     events: {
-      "click .list-group-item": "doSearch"
+      "click .list-group-item": "doSearch",
+      "change .form-group": "doSearch",
+      "change .form-control": "doSearch"
     },
 
     doSearch: function(e) {
+      var changed = $(e.currentTarget);
+
+      if (changed.prop("tagName") === "SELECT") { changed = changed.find(":selected"); }
+      else if (changed.attr("class") === "form-group") { changed = changed.find("input:checked")}
+      else if (changed.prop("tagName") === "INPUT") { changed.attr('value', changed.val()) }
+
       var querystring = 'query?';
       $('.results-list').empty();
 
-      if ($(e.currentTarget).attr('class') == 'list-group-item'){
-        if ($('.clicked').length > 0) {$('.clicked').removeClass('clicked');}
+      if (changed.attr('class') === 'list-group-item' || 'form-control') {
 
-        $(e.currentTarget).addClass('clicked');
-        querystring += $(e.currentTarget).attr('id') +'=' +$(e.currentTarget).attr('value');
+        if ($('.active').length > 0) {$('.active').removeClass('active');}
+
+        changed.addClass('active');
+        querystring += changed.attr('id') +'=' +changed.attr('value');
 
         $.ajax({method: 'GET',
                 url: '/api/catalog/'+querystring,
                 dataType: 'json',
                 context: this,
-                success: function(r){this.newCourseList(r);}
+                success: function(r) { this.newCourseList(r); }
               });
-        } else {$(e.currentTarget).removeClass('clicked');}
+        } else { changed.removeClass('active'); }
     },
 
     addList: function(course) {
       var reslist = this.$('.results-list');
-			var view = new app.CourseView({model: course});
+			var view = new app.CourseView( {model: course} );
 			this.$('.results-list').append(view.render().el);
 		},
 
-    newCourseList: function(list){
+    newCourseList: function(list) {
       var self = this;
-      list.forEach(function(elmt){
+      list.forEach(function(elmt) {
         elmt = new app.CourseModel({
           title: elmt.title,
           code: elmt.code,
