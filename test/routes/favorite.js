84833aa2ed18809c85ca7bae382062bbde079d12
Paolo Bernasconi <paolo@plaid.com>
2015-12-10 23:54:00 -0500

    unit test for authentication
    minor fixes to package.json for unit tests
    
    fix test

---
+++ test/routes/favorite.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/* global describe, it, before */
+
+var Promise = require('bluebird');
+
+var agent = require('../agent');
+
+
+var authedAgent;
+
+describe('/api/schedule', function() {
+
+  before(Promise.coroutine(function*(done) {
+    authedAgent = yield agent;
+  }));
+
+  it('GET / ', function(done) {
+    authedAgent
+      .get('/api/schedule')
+      .expect(200)
+      .end(function(err, res) {
+        // console.log(res.body);
+        done();
+      });
+  });
+
+  it('GET / ', function(done) {
+    authedAgent
+      .get('/api/schedule/1')
+      .expect(200)
+      .end(function(err, res) {
+        // console.log(res.body);
+        done();
+      });
+  });
+
+});
