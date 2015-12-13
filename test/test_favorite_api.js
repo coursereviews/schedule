79c3b39f975c49ba6ee11ad505988db64c327448
Julian Billings <jabillings@gmail.com>
2015-12-13 02:11:14 -0500

    add catalog tests. 1 is failing because of API. Others will pass if you run 'mocha test/routes/catalog.js', but NOT if you pass 'make test' as of now

--- test/test_favorite_api.js
+++ test/test_favorite_api.js
@@ -1,57 +1,13 @@
 'use strict';
 
-const app = require('../lib/server').app;
-const express = require('express');
-const favoriteAPI = require('../lib/routes/favorite/favorite.controller');
-const User = require('../lib/models/user');
-const assert = require('assert');
 // const request = require('supertest');
 const mocha = require('mocha');
 const child_process = require('child_process');
 const exec = child_process.exec;
-const execFile = child_process.execFile;
 const spawn = child_process.spawn;
 const termNo = '201590';
-
-
-/*
- NOTE: This test should work with the current Fall 2015 catalog that gets scraped.
- It will not work with other data.
- Additionally, this test code only works if the favorite table is already empty.
-
-*/
-
-
-var runFavoriteTests = function(email) {
-  return new Promise(function(resolve, reject) {
-    var user;
-    var usr = new User({
-        email: email
-      }).fetch()
-      .then(function(usr) {
-        user = usr;
-        //console.log(user);
-        return favoriteAPI.getAllFavoritesHelper(user);
-      }).then(function(result) {
-        assert.strictEqual(result.models.length, 0, "empty favorites DB not returning 0 results");
-        return favoriteAPI.addFavoriteHelper(user, 1);
-      }).then(function() {
-        return favoriteAPI.getAllFavoritesHelper(user);
-      }).then(function(result) {
-        assert.strictEqual(result.models.length, 1, "adding course doesn't work");
-        return favoriteAPI.removeFavoriteHelper(user, 1);
-      }).then(function() {
-        return favoriteAPI.getAllFavoritesHelper(user);
-      }).then(function(result) {
-        assert.strictEqual(result.models.length, 0, "removing course doesn't work");
-        resolve(true);
-      }).catch(function(err) {
-        console.log(err);
-        reject(Error(err));
-      });
-  });
-
-}
+const catalog = require('./routes/catalog');
+const base = require('./routes/base');
 
 // function clears database if it exists
 var clearDB = function() {
@@ -88,11 +44,6 @@ var changePermissions = function() {
 var makeMigrations = function() {
   return new Promise(function (resolve, reject) {
     exec('npm run migrate:latest', function(err, stdout, stderr) {
-      // idea for permission change from jansmolders86's reply to 'Running node (express)
-      // on linux produces Error: spawn EACCES' hosted at:
-      // http://stackoverflow.com/questions/19009778/ (cont.)
-      // running-node-express-on-linux-produces-error-spawn-eacces
-
       if (err){
         reject(err);
       }
@@ -136,6 +87,7 @@ clearDB()
   return populateDB();
 }).then(function(fulfill) {
   console.log('Success!');
+  catalog.runTests();
 }).catch(function(err) {
   console.log(err);
 });
